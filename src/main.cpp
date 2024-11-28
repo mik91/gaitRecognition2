f3faa698e9b2174a53aa9fd4c14465e8859a8e40
mik91 <41644495+mik91@users.noreply.github.com>
2024-11-27 19:35:04 -0500

    Setting up project

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <filesystem>
+namespace fs = std::filesystem;
+#include <iostream>
+#include <vector>
+
+
+int main() {
+    std::cout << "Loading images for subject test..." << std::endl;
+    std::vector<std::string> subjectIds = {"test", "test2", "test3"};
+
+    return 0;
+}
